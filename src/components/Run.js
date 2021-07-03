a7176e706a38282c844ed0730f52a5e43373c0ae
Elliot Plant <elliotaplant@gmail.com>
2021-07-02 17:16:22 -0700

    Maybe get counter working

--- src/components/Run.js
+++ src/components/Run.js
@@ -20,14 +20,15 @@ function stepsAfter(stepNumber, workout) {
 export default function Run({ workouts }) {
   const { workoutIndex } = useParams();
   const [stepNumber, setStepNumber] = useState(-1);
+  const [runHistory, setRunHistory] = useState({});
   const nextStep = () => setStepNumber(step => step + 1);
   const workout = workouts[workoutIndex];
 
   return <div className="run-container">
     <Link to="/workouts">Workouts</Link>
     {stepNumber === -1 && <FirstStep workout={workout} nextStep={nextStep} />}
-    {stepsWithin(stepNumber, workout) && <Step workout={workout} stepNumber={stepNumber} nextStep={nextStep} />}
-    {stepsAfter(stepNumber, workout) && <Finish workout={workout} />}
+    {stepsWithin(stepNumber, workout) && <Step workout={workout} stepNumber={stepNumber} nextStep={nextStep} runHistory={runHistory} setRunHistory={setRunHistory} />}
+    {stepsAfter(stepNumber, workout) && <Finish workout={workout} runHistory={runHistory} />}
   </div>;
 }
 
@@ -38,17 +39,17 @@ function FirstStep({ workout, nextStep }) {
   </>
 }
 
-function Step({ workout, nextStep, stepNumber }) {
+function Step({ workout, nextStep, stepNumber, runHistory, setRunHistory }) {
   const block = workout.blocks[stepNumber];
   return <>
     <h1>{block.exercise}</h1>
     {
       {
-        Reps: <RunReps block={block} />,
-        Timer: <RunTimer block={block} />,
-        AMRAP: <RunAMRAP block={block} />,
-        ALAP: <RunALAP block={block} />,
-        Rest: <RunRest block={block} />,
+        Reps: <RunReps block={block} stepNumber={stepNumber} />,
+        Timer: <RunTimer block={block} stepNumber={stepNumber} />,
+        AMRAP: <RunAMRAP block={block} stepNumber={stepNumber} runHistory={runHistory} setRunHistory={setRunHistory} />,
+        ALAP: <RunALAP block={block} stepNumber={stepNumber} runHistory={runHistory} setRunHistory={setRunHistory} />,
+        Rest: <RunRest block={block} stepNumber={stepNumber} />,
       }[block.type]
     }
     <p>{block.note}</p>
@@ -58,18 +59,19 @@ function Step({ workout, nextStep, stepNumber }) {
   </>
 }
 
-function Finish({ workout }) {
+function Finish({ workout, runHistory }) {
   return <>
     <h1>Finished {workout.name}!</h1>
+    {JSON.stringify(runHistory)}
     <Link to="/workouts">Back to workouts</Link>
   </>
 }
 
-function RepCounter() {
+function RepCounter({ blockIndex, onChange }) {
   const [reps, setRepCount] = useState(0);
 
   return <div>
-    Num Reps: <input value={reps} onChange={e => setRepCount(e.target.value)} />
+    Num Reps: <input name={blockIndex} value={reps} onChange={({ target: { value } }) => { onChange(value); setRepCount(value); }} />
   </div>
 }
 
@@ -83,21 +85,23 @@ function RunTimer({ block }) {
   </>
 }
 
-function RunAMRAP({ block }) {
+function RunAMRAP({ block, stepNumber, runHistory, setRunHistory }) {
+  const updateRun = (count) => setRunHistory(runHistory => ({ ...runHistory, [stepNumber]: { exercise: block.exercise, count } }));
   return <>
     {block.hasTimer && <Timer initialTime={block.timer} autoStart={block.autoStart} />}
-    {block.hasRepCounter && <RepCounter />}
+    {block.hasRepCounter && <RepCounter blockIndex={stepNumber} onChange={updateRun} />}
   </>
 }
 
-function RunALAP({ block }) {
+function RunALAP({ block, stepNumber, runHistory, setRunHistory }) {
+  const updateRun = (time) => setRunHistory(runHistory => ({ ...runHistory, stepNumber: { exercise: block.exercise, time } }));
   return <>
-    {block.hasTimeCounter && <TimeCounter autoStart={block.autoStart} />}
+    {block.hasTimeCounter && <TimeCounter autoStart={block.autoStart} stepNumber={stepNumber} onStop={updateRun} />}
   </>
 }
 
-function RunRest({ block }) {
+function RunRest({ block, stepNumber }) {
   return <>
-    <Timer initialTime={block.timer} autoStart={block.autoStart} />
+    <Timer initialTime={block.timer} autoStart={block.autoStart} stepNumber={stepNumber} />
   </>
 }
